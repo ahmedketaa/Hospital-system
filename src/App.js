dd9aaa23eb2259cb86db2d7cd10d4453ae747d71
andf.750@gmail.com <andf.750@gmail.com>
2024-09-02 02:51:59 +0300

    complete landing sections

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/auth/login/login";
 import RegisterPage from "./pages/auth/register/register";
-import DoctorProfile from "./pages/doctorProfle/doctorProfile";
 import Home from "./pages/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signin" element={<LoginPage />} />
-        <Route path="/signup" element={<RegisterPage />} />
-        <Route path="/doctorprofile/:id" element={<DoctorProfile />} />
         <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
       </Routes>
     </BrowserRouter>
   );
