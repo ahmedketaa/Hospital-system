ce2bc69e131e2f00b3ef5b42d980071b5931e470
Mariam Gamal <mariamgamal4646@gmail.com>
2024-09-01 17:47:39 +0300

    add header

--- src/blogs/blogs.js
+++ src/blogs/blogs.js
@@ -12,7 +12,28 @@ const Blogs = () => {
           style={{ height: "100%", width: "100%", objectFit: "cover" }}
         />
       </div>
-
+      <div
+        style={{
+          backgroundColor: "#eeeeee",
+          width: "1380px",
+          marginLeft: "-15px",
+          padding: "15px",
+        }}>
+        <div className="ms-5">
+          <p style={{ color: "grey", fontFamily: "-moz-initial" }}>
+            Home <span style={{ color: "#222f66" }}>{">"}Blog</span>
+          </p>
+          <p
+            style={{
+              marginTop: "-15px",
+              fontFamily: "-moz-initial",
+              fontSize: "23px",
+              color: "#29367d",
+            }}>
+            SAIFEE BLOG
+          </p>
+        </div>
+      </div>
       {/* ################ */}
 
       {/*cards */}
