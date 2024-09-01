9b15330f4fda1fa546f8253ee7bf19137a384c98
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-09-01 09:17:55 +0300

    landing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-
       </Routes>
     </BrowserRouter>
   );
