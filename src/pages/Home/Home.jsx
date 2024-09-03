4a83a7869267c33bdfcb913b90379ecfe7cb99de
ELWAZEIR <hossamshapan88@gmail.com>
2024-09-03 19:04:23 +0300

    back Header , SubNav,SubFooter,Footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import Header from '../../components/Header'
+import SubNav from '../../components/SubNav'
 import CustomDivWithCarousel from '../../components/Test'
 import SecondSection from '../../components/SecondSection'
 import ThirdSection from '../../components/ThirdSection'
 import AppointmentSection from '../../components/bookAppointment'
+import SubFooter from '../../components/SubFooter'
+import Footer from '../../components/Footer'
 
 export default function Home() {
   return (
     <div>
+      
        
         <CustomDivWithCarousel />
         <SecondSection />
         <ThirdSection />
         <AppointmentSection/>
      
+
     </div>
   )
 }
